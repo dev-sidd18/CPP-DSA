9e927b824499d943309578d875302e3a7be33e36
dev-sidd18 <siddharthakumarshakya.2108@gmail.com>
2026-09-04 00:28:36 +0530

    add queue

---
+++ queue/ins_del.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int myqueue[100];
+int top=-1;
+int rear=-1;
+void ins(int x){
+    if(top==n-1){
+        cout<<"overflow";
+    }
+    else{
+        
+    }
+}
+int main(){
+
+}
