4168cdc775ee5d09c3a16c026cb331de324ed2c8
dev-sidd18 <siddharthakumarshakya.2108@gmail.com>
2026-09-22 23:03:10 +0530

    solve leetcode 102 problme

---
+++ problems/leetcode/102_level_order_traversal.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(root==NULL){
+            return ans;
+        }
+        else{
+            queue<TreeNode*>q;
+            q.push(root);
+            while(!q.empty()){
+                vector<int>visited;
+                int level=q.size();
+                for(int i=0;i<level;i++){
+                    TreeNode* curr=q.front();
+                    q.pop();
+                    visited.push_back(curr->val);
+                    if(curr->left!=NULL){
+                        q.push(curr->left);
+                    }
+                    if(curr->right!=NULL){
+
+                        q.push(curr->right);
+                    }
+                }
+                ans.push_back(visited);
+            }
+        }
+        return ans;
+    }
+};
